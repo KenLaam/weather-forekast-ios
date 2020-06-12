ec885defd656f32b2b6d56be81b8f9c2440fa000
Ken Lâm <hoangvu23108@gmail.com>
2020-06-13 01:43:46 +0700

    Add simple test

---
+++ WeatherForekastTests/ExtensionTests.swift
@@ -0,0 +1,28 @@
+//
+//  ExtensionTests.swift
+//  WeatherForekastTests
+//
+//  Created by Ken Lâm on 6/13/20.
+//  Copyright © 2020 Ken Lam. All rights reserved.
+//
+
+import XCTest
+@testable import WeatherForekast
+
+class ExtensionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testClassName() throws {
+        XCTAssertEqual(HomeViewController.className, "HomeViewController")
+    }
+    
+    func testDateTime() {
+    }
+}
